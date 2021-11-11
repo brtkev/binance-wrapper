d32bb4d76fe189859dd0c5b1fa9d70ae769fa1c2
kev <kbreto2911@gmail.com>
2021-11-11 19:01:08 -0400

    symbolHistorialKlines time to string

--- __tests__/extra.py
+++ __tests__/extra.py
@@ -7,8 +7,7 @@ class Extra(unittest.TestCase):
 
     def symbolHistorialKlines(self):
         import time
-
-        startTime =  int(time.mktime(time.strptime('10 oct 2020', "%d %b %Y")))
-        endTime = int(time.mktime(time.strptime('5 mar 2021', "%d %b %Y")))
+        startTime = '10 oct 2020'
+        endTime = '5 mar 2021'
         res = binanceWrapper.extra.symbolHistorialKlines('XRPUSDT', '1h', startTime, endTime)
         self.assertGreater(len(res), 500)
--- binanceWrapper/extra.py
+++ binanceWrapper/extra.py
@@ -1,7 +1,7 @@
-import binanceWrapper
+import binanceWrapper, time
 
 
-def symbolHistorialKlines(symbol : str, interval : str, startTime : int , endTime : int ):
+def symbolHistorialKlines(symbol : str, interval : str, startTime : str , endTime : str ):
     """fetch klines from startTime to endTime\n"""
     def filterDups(history : list[list]):
         filter = set()
@@ -10,8 +10,8 @@ def symbolHistorialKlines(symbol : str, interval : str, startTime : int , endTim
                 filter.add(bar[0])
                 yield bar 
 
-    startTime *= 1000
-    endTime *= 1000
+    startTime =  int(time.mktime(time.strptime(startTime, "%d %b %Y"))) * 1000
+    endTime = int(time.mktime(time.strptime( endTime, "%d %b %Y"))) * 1000
     klines = []
     while startTime < endTime:
         klines.extend(binanceWrapper.symbolKlines(symbol, interval, 1000, startTime))
